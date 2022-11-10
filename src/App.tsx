f07bf3f4a44ff4cafbf25b96dc541fb90c5cfc1d
Trapti Rahangdale <imtrapti1410@gmail.com>
2022-11-10 17:57:12 +0530

    shopping cart layout

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,6 @@ function App() {
     <h1 className="sidebar">TeeRex Store</h1>
     <nav>
      <a href="/">Products</a>
-     {/* <a href="/style-guide">style-guide</a> */}
     </nav>
    </header>
 
@@ -185,6 +184,29 @@ function App() {
       />
      </svg>
     </button>
+   </div>
+   <div className="shopping-cart-page">
+    <h2>Shopping Cart</h2>
+    <ul className="shopping-cart__list">
+     <li className="shopping-cart__item">
+      <img
+       className="product__image"
+       src="https://via.placeholder.com/300"
+       alt=""
+      />
+      <div className="shopping-cart__item-desc">
+       <span>Name</span>
+       <span>Rs 300</span>
+      </div>
+
+      <select name="qty" id="qty">
+       <option value="1">Qty: 1</option>
+       <option value="2">Qty: 2</option>
+      </select>
+
+      <button>Delete</button>
+     </li>
+    </ul>
    </div>
   </div>
  );

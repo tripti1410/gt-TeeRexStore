3efc7c7278a2317513c05eaa05fb7d368f63ee25
Trapti Rahangdale <imtrapti1410@gmail.com>
2022-11-11 13:00:51 +0530

    refactored all the markyp and css into function component

---
+++ src/components/header/header.tsx
@@ -0,0 +1,15 @@
+function Header() {
+ return (
+  <header className="with-sidebar">
+   <h1 className="sidebar">
+    TeeRex
+    <span>Store</span>
+   </h1>
+   <nav>
+    <a href="/">Products</a>
+   </nav>
+  </header>
+ );
+}
+
+export default Header;
---
+++ src/pages/shopping-cart/shopping-cart.tsx
@@ -0,0 +1,29 @@
+import "./shoppoing-cart.scss";
+function ShoppingCart() {
+ return (
+  <div className="shopping-cart-page">
+   <h2>Shopping Cart</h2>
+   <ul className="shopping-cart__list">
+    <li className="shopping-cart__item">
+     <img
+      className="product__image"
+      src="https://via.placeholder.com/300"
+      alt=""
+     />
+     <div className="shopping-cart__item-desc">
+      <span>Name</span>
+      <span>Rs 300</span>
+     </div>
+
+     <select name="qty" id="qty">
+      <option value="1">Qty: 1</option>
+      <option value="2">Qty: 2</option>
+     </select>
+
+     <button>Delete</button>
+    </li>
+   </ul>
+  </div>
+ );
+}
+export default ShoppingCart;
